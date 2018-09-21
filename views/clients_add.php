8eb99d5b6f18a3308b4c43068d047a17ff32836d
WB <WB@10.0.0.105>
2018-09-20 21:31:15 -0300

    Alteração em Clientes

--- views/clients_add.php
+++ views/clients_add.php
@@ -15,6 +15,7 @@
 	<label for="phone">Telefone</label><br/>
 	<input type="text" name="phone" /><br/><br/>
 
+        
 	<label for="internal_obs">Observações Internas</label><br/>
 	<textarea name="internal_obs" id="internal_obs"></textarea><br/><br/>
 
